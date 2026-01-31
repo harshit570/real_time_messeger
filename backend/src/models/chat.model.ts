1ad936b0df07e1f3decb4818573cc87a79964129
harshit570 <harshitsingh18032005@gmail.com>
2026-01-31 10:10:28 +0530

    first

---
+++ backend/src/models/chat.model.ts
@@ -0,0 +1,30 @@
+
+import mongoose ,{ Document, Schema } from 'mongoose';
+
+export interface ChatDocument extends Document {
+  participants: mongoose.Types.ObjectId[];
+  lastmessage:mongoose.Types.ObjectId,
+  isGroup:boolean,
+  groupName:string,
+  createdBy:mongoose.Types.ObjectId,
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const chatSchema=new Schema<ChatDocument>(
+  {
+    participants: [
+      { type: Schema.Types.ObjectId, ref: 'User', required: true }
+    ],
+    lastmessage: { type: Schema.Types.ObjectId, ref: 'Message', default: null },
+    isGroup: { type: Boolean, default: false },
+    groupName: { type: String, default: null },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  },
+  {
+    timestamps:true
+  }
+)
+
+const ChatModel=mongoose.model<ChatDocument>("Chat",chatSchema);  
+export default ChatModel;
