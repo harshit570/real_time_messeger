014d77d4ddca0e4a22a707db670657bee5041c33
harshit570 <harshitsingh18032005@gmail.com>
2026-02-06 17:13:35 +0530

    frontend

---
+++ client/src/components/app-wrapper.tsx
@@ -0,0 +1,14 @@
+
+interface Props{
+  children:React.ReactNode
+}
+const AppWrapper = ({children}:Props) => {
+  return (
+    <div className="h-full">
+      {/* Toolbar */}
+      <main className=" h-full">{children}</main>
+    </div>
+  )
+}
+
+export default AppWrapper;
---
+++ client/src/pages/chat/index.tsx
@@ -0,0 +1,8 @@
+
+const Chat = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Chat;
